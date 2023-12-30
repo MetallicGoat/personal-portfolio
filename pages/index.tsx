157ec144c6c2ac0b34c636789e83ab7104358025
MetallicGoat <the.great.metallic.goat@gmail.com>
2023-12-29 23:16:15 -0500

    even more styling fixes x2

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home() {
                     animate={{opacity: 1, y: 0}}
                     transition={{duration: .3}}
                 >
-                    <div className="text-center md:w-3/5 md:text-left pb-2 md:pb-0">
+                    <div className="text-center md:w-3/5 md:text-left py-2 md:pb-0">
                         <h2
                             className="text-4xl lg:text-5xl xl:text-6xl sm:py-2 font-extrabold animated-text-color"
                         >
@@ -53,7 +53,7 @@ export default function Home() {
                     </div>
 
                     <div
-                        className="flex justify-center md:justify-end mx-auto md:mx-0 relative py-3 w-3/4 md:w-1/3"
+                        className="flex justify-center md:justify-end mx-auto md:mx-0 relative pt-2 w-3/4 md:w-1/3"
                     >
                         <InteractiveImage
                             src="https://github.com/MetallicGoat.png"
@@ -66,7 +66,7 @@ export default function Home() {
 
                     {/* Scroll Arrow */}
                     <div
-                        className="pt-4 mx-auto md:absolute md:bottom-5 md:right-0 md:left-1/2 md:transform md:-translate-x-1/2"
+                        className="pt-2 mx-auto md:absolute md:bottom-5 md:right-0 md:left-1/2 md:transform md:-translate-x-1/2"
                     >
                         <BsChevronDoubleDown
                             className="mx-auto animate-bounce text-4xl md:text-5xl font-bold text-gray-600 dark:text-gray-400"
@@ -96,7 +96,7 @@ const Socials: FunctionComponent<HashTagProps> = ({className}) => {
     return (
         <div className={className}>
             <div
-                className="text-4xl md:text-5xl lg:text-6xl flex justify-center gap-8 sm:gap-16 my-3 py-2 md:py-6 text-gray-600 dark:text-gray-400"
+                className="text-4xl md:text-5xl lg:text-6xl flex justify-center gap-10 sm:gap-16 my-3 py-2 md:py-6 text-gray-600 dark:text-gray-400"
             >
 
                 <a href="https://github.com/MetallicGoat" target="blank_">
